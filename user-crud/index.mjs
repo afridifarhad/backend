54f30213d357ac7e9aac403cbc2ad24e2983cdc7
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-21 21:45:33 +0500

    create a server where server is running port no 3000

--- user-crud/index.mjs
+++ user-crud/index.mjs
@@ -1,4 +1,11 @@
 import express from "express"
 
 const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening here http://localhost:${PORT}`)
+    
+})
+
 
