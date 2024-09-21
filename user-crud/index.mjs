36eec278bd7fa1bf73ef63d824e6dee39eb9b088
Farhad Afridi <afridifarhad149@gmail.com>
2024-09-21 22:23:04 +0500

    add user in an array

--- user-crud/index.mjs
+++ user-crud/index.mjs
@@ -3,9 +3,26 @@ import express from "express"
 const app = express()
 const PORT = 3000
 
+const users = [
+    {
+        id: 1,
+        name: "Farhad Ullah",
+        email: "farhad@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Afzal Khan",
+        email: "afzalkhan@gmail.com"
+    }
+]
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening here http://localhost:${PORT}`)
-    
+
 })
 
 
